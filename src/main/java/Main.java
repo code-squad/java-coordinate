6c29fb0565b932ee1db3484a1eb8806171235edc
jimmy <foodsksms@gmail.com>
2018-03-09 11:25:27 +0900

    피드백 완료

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,28 +2,12 @@ import java.util.List;
 
 public class Main {
     public static void main (String [] args){
-
         Input.firstInput();
         String pointSet = Input.pointSet();
         List<String> points = Input.pointInput(pointSet);
-
         int numberOfPoints = points.size();
 
-        if(points.size() == 2) {
-            Line line = new Line(points);
-            ResultView.printResult(line);
-        }
-
-        if(points.size() == 3) {
-            Triangle triangle = new Triangle(points);
-            ResultView.printResult(triangle);
-        }
-
-        if(points.size() == 4) {
-            Square square = new Square(points);
-            ResultView.printResult(square);
-        }
-
+        ResultView.printResult(numberOfPoints, points);
         Draw draw = new Draw(points);
         draw.drawline(numberOfPoints);
         System.out.print(String.format("%3s","+"));
