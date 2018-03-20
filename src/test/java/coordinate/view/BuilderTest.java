a392e35be8aa6a0e74aba3a9f90aac8aafc7b03c
krapeaj <jaeepark@gmail.com>
2018-03-20 12:29:53 +0900

    Finish junit test for Builder

--- src/test/java/coordinate/view/BuilderTest.java
+++ src/test/java/coordinate/view/BuilderTest.java
@@ -13,39 +13,55 @@ public class BuilderTest {
     }
 
     @Test
-    public void addYLabel() {
-        assertEquals("2", Builder.addYLabel(2)); //짝수
-        assertEquals(" ", Builder.addYLabel(1)); //홀수
+    public void addYLabelEvenNumber() {
+        assertEquals("2", addYLabel(2)); //짝수
     }
 
     @Test
-    public void formatRow() {
+    public void addYLabelOddNumber() {
+        assertEquals(" ", addYLabel(1)); //홀수
+    }
+
+    @Test
+    public void formatRowTest() {
         String row = ".........."; //length = 10
         String expected = "                                                  "; //expected length = 50 = 60 - row.length()
-        assertEquals(expected, Builder.formatRow(row));
+        assertEquals(expected, formatRow(row));
     }
 
     @Test
-    public void addPoint() {
-        assertEquals(" *", Builder.addPoint(true, 10, 10)); //point is marked at some coordinate & not on any axis
-        assertEquals("  ", Builder.addPoint(false, 10, 10)); //point is not marked at some coordinate & not on any axis
-
-        assertEquals(" *", Builder.addPoint(true, 0, 0)); //point is marked at origin (0,0)
-        assertEquals(" +", Builder.addPoint(false, 0, 0)); //point is not marked at (0,0)
-
-        assertEquals(" *", Builder.addPoint(true, 1, 0)); //point is marked on x-axis (1,0)
-        assertEquals(" -", Builder.addPoint(false, 1, 0)); //point is not marked on x-axis (1,0)
-
-        assertEquals(" *", Builder.addPoint(true, 0, 1)); //point is marked on y-axis (0,1)
-        assertEquals(" |", Builder.addPoint(false, 0, 1)); //point is not marked on y-axis (0,1)
+    public void addPointNotOnAxis() {
+        assertEquals(" *", addPoint(true, 10, 10)); //point is marked at some coordinate & not on any axis
+        assertEquals("  ", addPoint(false, 10, 10)); //point is not marked at some coordinate & not on any axis
     }
 
     @Test
-    public void addXLabelNumber() {
-        assertEquals(" 8", Builder.addXLabelNumber(8)); //even number less than 2 digits
-        assertEquals("10", Builder.addXLabelNumber(10)); //even number >= 2 digits
+    public void addPointOnYAxis() {
+        assertEquals(" *", addPoint(true, 0, 1)); //point is marked on y-axis (0,1)
+        assertEquals(" |", addPoint(false, 0, 1)); //point is not marked on y-axis (0,1)
+    }
 
-        assertEquals("  ", Builder.addXLabelNumber(7)); //odd number less than 2 digits
-        assertEquals("  ", Builder.addXLabelNumber(15)); //odd number >= 2 digits
+    @Test
+    public void addPointOnXAxis() {
+        assertEquals(" *", addPoint(true, 1, 0)); //point is marked on x-axis (1,0)
+        assertEquals(" -", addPoint(false, 1, 0)); //point is not marked on x-axis (1,0)
+    }
+
+    @Test
+    public void addPointOnOrigin() {
+        assertEquals(" *", addPoint(true, 0, 0)); //point is marked at origin (0,0)
+        assertEquals(" +", addPoint(false, 0, 0)); //point is not marked at (0,0)
+    }
+
+    @Test
+    public void addXLabelEvenNumber() {
+        assertEquals(" 8", addXLabelNumber(8)); //even number less than 2 digits
+        assertEquals("10", addXLabelNumber(10)); //even number >= 2 digits
+    }
+
+    @Test
+    public void addXLabelOddNumber() {
+        assertEquals("  ", addXLabelNumber(7)); //odd number less than 2 digits
+        assertEquals("  ", addXLabelNumber(15)); //odd number >= 2 digits
     }
 }
