84ae9a923c522e3566a2c6bf723b682a225922ae
david-learner <xodnjs2549@naver.com>
2018-03-21 19:37:19 +0900

    save. global user email

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -12,21 +12,21 @@ public class ResultView {
 
     private static void printXAxis() {
         System.out.print("  " + "+");
-        for (int i = 0; i <MAX ; i++) {
+        for (int i = 0; i < MAX; i++) {
             System.out.print(String.format("%2s", "--"));
         }
         System.out.print("\n" + String.format("%-3s", "0"));
         for (int i = 1; i < 13; i++) {
-            System.out.print(String.format("%4s", i*2));
+            System.out.print(String.format("%4s", i * 2));
         }
     }
 
     private static int printYAxis(int index) {
         if (index % 2 == 0) {
-            System.out.println(String.format("%-2s"+"|", index));
+            System.out.println(String.format("%-2s" + "|", index));
             return index;
         }
-        System.out.println(String.format("%-2s"+"|", "  "));
+        System.out.println(String.format("%-2s" + "|", "  "));
         return index;
     }
 }
