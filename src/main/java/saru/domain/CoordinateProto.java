1bf7cbe20886a495b7a68db079d98e0b17c047d4
saru <sarguments@gmail.com>
2018-03-21 13:51:11 +0900

    constant number add

--- src/main/java/saru/domain/CoordinateProto.java
+++ src/main/java/saru/domain/CoordinateProto.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 public class CoordinateProto {
     private static final int LIMIT = 25;
+    public static final String VERTICAL_SYMBOL = "|";
+    public static final String ANCHOR_POINT = "+";
+    public static final String HORIZONTAL_SYMBOL = "--";
+    public static final int EVEN = 2;
+    public static final String BLANK = "  ";
 
     public CoordinateProto() {
         // empty
@@ -19,7 +24,7 @@ public class CoordinateProto {
         List<Integer> genList = generateReverse(0, limit - 1);
         for (int i : genList) {
             System.out.printf("%2d|\n", i);
-            System.out.printf("%3s\n", "|");
+            System.out.printf("%3s\n", VERTICAL_SYMBOL);
         }
     }
 
@@ -29,7 +34,7 @@ public class CoordinateProto {
     }
 
     private static void printHorizontalNum(int limit) {
-        System.out.printf("%2d  ", 0);
+        System.out.printf("%2d" + BLANK, 0);
 
         List<Integer> genList = generate(0, limit - 1);
         for (int i : genList) {
@@ -38,15 +43,15 @@ public class CoordinateProto {
     }
 
     private static void printHorizontalLine(int limit) {
-        System.out.printf("%3s", "+");
+        System.out.printf("%3s", ANCHOR_POINT);
         for (int i = 0; i < limit; i++) {
-            System.out.print("--");
+            System.out.print(HORIZONTAL_SYMBOL);
         }
         System.out.println();
     }
 
     private static void insertNum(List<Integer> resultList, int i) {
-        if (i % 2 == 0) {
+        if (i % EVEN == 0) {
             resultList.add(i);
         }
     }
