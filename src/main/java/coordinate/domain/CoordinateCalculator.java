98baa8d3732a9c324b3a2e308f01337e30e6cf74
krapeaj <jaeepark@gmail.com>
2018-03-22 11:29:00 +0900

    Create calculatorInit that initializes list of rows

--- src/main/java/coordinate/domain/CoordinateCalculator.java
+++ src/main/java/coordinate/domain/CoordinateCalculator.java
@@ -7,17 +7,23 @@ import java.util.List;
 
 public class CoordinateCalculator {
     private static final int RANGE = 24;
-    private List<Row> rows = new ArrayList<>();
+    private List<Row> rows;
     private final Points points;
 
     public CoordinateCalculator(Points points) {
         this.points = points;
+        this.rows = calculatorInit(points);
+    }
+
+    public static List<Row> calculatorInit(Points points) {
+        List<Row> rows = new ArrayList<>();
         for (int y = 0; y <= RANGE; y++) {
             rows.add(addRow(points, y));
         }
+        return rows;
     }
 
-    private Row addRow(Points points, int y) {
+    private static Row addRow(Points points, int y) {
         return new Row(points.getXOnRowY(y)); //point가 있는 y번째 row에 x좌표
     }
 
