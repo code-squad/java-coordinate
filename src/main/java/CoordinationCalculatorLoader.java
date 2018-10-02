dec7909ea53a7178966a1de1e144f242d1f1bf42
NightNineNeighbor <dud9203@gmail.com>
2018-10-02 09:52:57 +0900

    step1 (#57)
    * step1
    
    * step1

---
+++ src/main/java/CoordinationCalculatorLoader.java
@@ -0,0 +1,7 @@
+public class CoordinationCalculatorLoader {
+    public static void main(String[] args) {
+        GridPaper gridPaper = new GridPaper(24);
+        gridPaper.draw();
+        System.out.println(gridPaper);
+    }
+}
