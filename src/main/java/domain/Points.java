a769045fd586a7568415fed3462c3d368524d2d4
sechun <sechun0215@gmail.com>
2018-10-07 20:07:37 +0900

    step3 first commit

--- src/main/java/domain/Points.java
+++ src/main/java/domain/Points.java
@@ -21,27 +21,53 @@ public class Points {
     public List<Integer> makeLine(int y) {
         List<Integer> points = new ArrayList<>();
         for (int i = 0; i <= Point.MAXSIZE; i++) {
-            points.add(searchSameXPoint(i, y));
+            points.add(searchSamePoint(i, y));
         }
         return points;
     }
 
-    private int searchSameXPoint(int x, int y) {
+    private int searchSamePoint(int x, int y) {
+        Point tmp = Point.init(x, y);
+
         for (Point p : this.points) {
-            if (p.isSameX(x) && p.isSameY(y))
+            if (p.equals(tmp))
                 return EXIST;
         }
         return NONE;
     }
 
-    public double calc() {
-        if (this.points.size() == 2) {
-            return getDistance();
-        }
-        return 0;
+    public Boolean isThis(int figure) {
+        return this.points.size() == figure;
     }
 
     public double getDistance() {
         return this.points.get(0).getDistance(this.points.get(1));
     }
+
+    public Point searchSameYPoint(Point p) throws Exception {
+        for (int i = 1; i < this.points.size(); i++) {
+            if (p.isSameY(this.points.get(i))) return this.points.get(i);
+        }
+        throw new Exception();
+    }
+
+    public Point searchSameXPoint(Point p) throws Exception {
+        for (int i = 1; i < this.points.size(); i++) {
+            if (p.isSameX(this.points.get(i))) return this.points.get(i);
+        }
+        throw new Exception();
+    }
+
+    public int getRectangularArea() {
+        try {
+            Point p = this.points.get(0);
+            int horizontal = (int) p.getDistance(searchSameYPoint(p));
+            int vertical = (int) p.getDistance(searchSameXPoint(p));
+
+            return horizontal * vertical;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
 }
