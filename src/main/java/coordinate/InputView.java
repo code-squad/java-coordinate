1a655bcef07c8002e0e5af9a032cee8ca4926ff4
kkssry <kkssry@naver.com>
2018-10-09 18:54:22 +0900

    submit step2 1st

--- src/main/java/coordinate/InputView.java
+++ src/main/java/coordinate/InputView.java
@@ -1,4 +1,26 @@
 package coordinate;
 
+import java.util.*;
+
 public class InputView {
+    private static StringTokenizer STZ;
+
+    public static String inputPoint() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("좌표를 입력하세요.");
+        return scan.next();
+    }
+
+    public static List<Integer> extractNum(String beforeToken) {
+        List<Integer> numbers = new ArrayList<>();
+        STZ = new StringTokenizer(beforeToken, "\\(|\\)|,|-");
+        while (STZ.hasMoreElements()) {
+            numbers.add(stringToInt(STZ.nextToken()));
+        }
+        return numbers;
+    }
+
+    public static int stringToInt(String stringObject) {
+        return Integer.parseInt(stringObject);
+    }
 }
--- src/main/java/coordinate/Point.java
+++ src/main/java/coordinate/Point.java
@@ -1,4 +1,29 @@
 package coordinate;
 
 public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y)throws IllegalArgumentException {
+        if (x < 0 || x > 24 || y < 0 || y > 24)
+            throw new IllegalArgumentException();
+        this.x = x;
+        this.y = y;
+    }
+
+    public double CalculatorLength(Point points) {
+        return Math.sqrt(Math.pow(this.x - points.x, 2) + Math.pow(this.y - points.y, 2));
+    }
+
+    public Boolean isX(int x) {
+        if (this.x * 3 == x)
+            return true;
+        return false;
+    }
+
+    public Boolean isY(int y) {
+        if (this.y == y)
+            return true;
+        return false;
+    }
 }
