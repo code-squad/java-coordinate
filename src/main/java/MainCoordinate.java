aefd65d77b2ccbd2921ece834a363a6f5953c0e5
tskim <lollolzkk@gmail.com>
2018-10-10 10:36:44 +0900

    step1 complete

---
+++ src/main/java/MainCoordinate.java
@@ -0,0 +1,9 @@
+import static View.CoordinateView.drawXLine;
+import static View.CoordinateView.drawYLine;
+
+public class MainCoordinate {
+    public static void main(String[] args) {
+        drawYLine();
+        drawXLine();
+    }
+}
