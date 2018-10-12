241db936a473ee9167b1787316c04ca32b686e86
ChoiSing <chltmdals115@gmail.com>
2018-10-12 18:20:41 +0900

    step6, complete. (#101)
    * step5, complete
    
    * modify protected variable
    
    * step6, complete

--- src/main/java/coordinate/domain/Figure.java
+++ src/main/java/coordinate/domain/Figure.java
@@ -2,7 +2,7 @@ package coordinate.domain;
 
 import java.util.List;
 
-public abstract class Figure {
+public abstract class Figure implements Area{
     private List<Point> points;
 
     public Figure(List<Point> points) {
@@ -13,13 +13,7 @@ public abstract class Figure {
         return this.points.get(index);
     }
 
-    protected int getPointsSize(){
-        return this.points.size();
-    }
-
     protected abstract boolean isValid();
-
-    public abstract double area();
 
     public abstract String getName();
 }
