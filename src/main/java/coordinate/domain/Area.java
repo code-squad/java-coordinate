60baa81b217184c00ac36bec95f3cff2ffb8699e
Choi-dongsun <rovks91@gmail.com>
2018-11-12 03:06:13 +0900

    step6 구현

---
+++ src/main/java/coordinate/domain/Area.java
@@ -0,0 +1,5 @@
+package coordinate.domain;
+
+public interface Area {
+    double getArea();
+}
--- src/main/java/coordinate/domain/Figure.java
+++ src/main/java/coordinate/domain/Figure.java
@@ -2,12 +2,10 @@ package coordinate.domain;
 
 import java.util.List;
 
-public abstract class Figure {
+public abstract class Figure implements Area{
     protected List<Point> coordinate;
 
     protected Figure(List<Point> coordinate){
         this.coordinate = coordinate;
     }
-
-    public abstract double getArea();
 }
