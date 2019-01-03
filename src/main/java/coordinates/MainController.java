68ac6a2b888c478672994c5400abd94b5ced8271
LYJr <lyi91@naver.com>
2019-01-04 02:53:37 +0900

    step1

---
+++ src/main/java/coordinates/MainController.java
@@ -0,0 +1,11 @@
+package coordinates;
+
+public class MainController {
+
+    private static CoordinatesServes coordinatesServes = new CoordinatesServes();
+
+    public static void main(String[] args) {
+        coordinatesServes.main();
+
+    }
+}
